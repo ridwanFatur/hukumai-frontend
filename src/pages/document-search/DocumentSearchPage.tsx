7774a047a123d0be107bf37d21dcaab808d81401
ridwanFatur <ridwan.faturrahnman.godjali@gmail.com>
2026-03-19 23:51:13 +0700

    Update

--- src/pages/document-search/DocumentSearchPage.tsx
+++ src/pages/document-search/DocumentSearchPage.tsx
@@ -1,4 +1,6 @@
+import { useGlobal } from "@/global-context/global";
 import DocumentSearchPageProvider from "./document-search-page-context";
+import { MenuIcon } from "lucide-react";
 
 export default function DocumentSearchPage() {
 	return (
@@ -9,7 +11,14 @@ export default function DocumentSearchPage() {
 }
 
 function _DocumentSearchPage() {
+	const { setIsMobileSidebarOpen } = useGlobal();
+
 	return <div className="w-full h-full relative">
-		Search
+		<button
+			className="md:hidden text-xl cursor-pointer transition p-4 absolute top-0 left-0 border border-white/30 rounded-md"
+			onClick={() => setIsMobileSidebarOpen(true)}
+		>
+			<MenuIcon size={20} />
+		</button>
 	</div>
 }
