483ce75407b96d45fb3d9f506dd8698f11d421e7
ridwanFatur <ridwan.faturrahnman.godjali@gmail.com>
2026-03-19 20:49:23 +0700

    Update Layout

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,26 +1,29 @@
 import { Outlet } from "react-router-dom";
 import { useGlobal } from "@/global-context/global";
-import Header from "./components/Header";
-import MobileSidebar from "./components/MobileSiedbar";
-import Footer from "./components/Footer";
 import LogoutDialog from "@/components/LogoutDialog";
+import Sidebar from "./components/Sidebar";
+import MobileSidebar from "./components/MobileSidebar";
 
 export default function MainLayout() {
 	const { isLogoutOpen, setIsLogoutOpen, handleLogout } = useGlobal();
 
 	return (
-		<div className={`min-h-dvh flex flex-col bg-[#f7f8fc]`}>
-			<Header />
-			<MobileSidebar />
-			<main className="flex-1 max-w-6xl w-full mx-auto px-6 py-10">
-				<Outlet />
-			</main>
-			<Footer />
+		<>
+			<div className="h-dvh w-full flex flex-col">
+				<div className="flex-1 w-full flex min-h-0">
+					<Sidebar />
+					<MobileSidebar />
+					<div className="flex-1">
+						<Outlet />
+					</div>
+				</div>
+			</div>
 			<LogoutDialog
 				isOpen={isLogoutOpen}
 				onCancel={() => setIsLogoutOpen(false)}
 				onLogout={handleLogout}
 			/>
-		</div>
-	);
+		</>
+	)
 }
+
