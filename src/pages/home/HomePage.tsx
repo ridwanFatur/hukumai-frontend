51d623c1a85cb867e26ef9a6a9bd93738b61d6e9
ridwanFatur <ridwan.faturrahnman.godjali@gmail.com>
2026-03-19 21:56:33 +0700

    Update

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,4 +1,6 @@
+import { useGlobal } from "@/global-context/global";
 import HomePageProvider from "./home-page-context"
+import { MenuIcon } from "lucide-react";
 
 export default function HomePage() {
 	return (
@@ -9,7 +11,14 @@ export default function HomePage() {
 }
 
 function _HomePage() {
+	const { setIsMobileSidebarOpen } = useGlobal();
+
 	return <div className="w-full h-full relative">
-		Home
+		<button
+			className="md:hidden text-xl cursor-pointer transition p-4 absolute top-0 left-0 border border-white/30 rounded-md"
+			onClick={() => setIsMobileSidebarOpen(true)}
+		>
+			<MenuIcon size={20} />
+		</button>
 	</div>
 }
