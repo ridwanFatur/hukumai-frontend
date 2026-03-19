0e2a72760f0e64b6ad922c08e49127d94587f321
ridwanFatur <ridwan.faturrahnman.godjali@gmail.com>
2026-03-20 00:00:37 +0700

    Update

--- src/layouts/components/DocumentSearchContent.tsx
+++ src/layouts/components/DocumentSearchContent.tsx
@@ -1,9 +1,8 @@
-import { useGlobal } from "@/global-context/global"
-
 export default function DocumentSearchContent({ alwaysOpen = false }: { alwaysOpen: boolean }) {
+
 	return <>
 		<div className="flex-1 overflow-y-auto">
-
+			{alwaysOpen && <div></div>}
 		</div>
 	</>
 }
