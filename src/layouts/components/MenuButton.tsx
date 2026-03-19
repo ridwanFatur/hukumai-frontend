b626c259cd5b0377cf41c33951d6b4b29fc54611
ridwanFatur <ridwan.faturrahnman.godjali@gmail.com>
2026-03-19 22:11:11 +0700

    Update

--- src/layouts/components/MenuButton.tsx
+++ src/layouts/components/MenuButton.tsx
@@ -1,29 +1,24 @@
 import { type LucideIcon } from "lucide-react"
-
 interface MenuButtonProps {
 	text?: string;
 	Icon?: LucideIcon;
 	color?: {
-		from: string;
-		to: string;
-		fromHover?: string;
-		toHover?: string;
-		shadow?: string;
+		bg: string;
+		hover: string;
+		shadow: string;
 	};
 	isSidebarOpen?: boolean;
 	onClick?: () => void;
-	size?: number; // ukuran icon
+	size?: number;
 }
 
 export default function MenuButton({
 	text,
 	Icon,
 	color = {
-		from: "violet-600",
-		to: "indigo-600",
-		fromHover: "violet-500",
-		toHover: "indigo-500",
-		shadow: "violet-900/30",
+		bg: "bg-gradient-to-r from-violet-600 to-indigo-600",
+		hover: "hover:from-violet-500 hover:to-indigo-500",
+		shadow: "shadow-violet-900/30",
 	},
 	isSidebarOpen = true,
 	onClick,
@@ -33,10 +28,16 @@ export default function MenuButton({
 		<button
 			title={isSidebarOpen ? undefined : text}
 			onClick={onClick}
-			className={`cursor-pointer flex items-center justify-center gap-2 ${isSidebarOpen ? "w-full py-2 px-3.5" : "w-9 h-9"
-				} rounded-xl bg-linear-to-r from-${color.from} to-${color.to} hover:from-${color.fromHover
-				} hover:to-${color.toHover} text-white text-xs font-semibold shadow-md shadow-${color.shadow
-				} transition-all duration-200 active:scale-95`}
+			className={`
+        cursor-pointer flex items-center justify-center gap-2
+        ${isSidebarOpen ? "w-full py-2 px-3.5" : "w-9 h-9"}
+        rounded-xl
+        ${color.bg}
+        ${color.hover}
+        text-white text-xs font-semibold
+        shadow-md ${color.shadow}
+        transition-all duration-200 active:scale-95
+      `}
 		>
 			{Icon && <Icon size={size} />}
 			{isSidebarOpen && text && (
